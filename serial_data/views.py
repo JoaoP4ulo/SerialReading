1ad6a3d3409bab2782679a722ae48d99b6aa815d
JoaoPaulo <jppelioterio@gmail.com>
2023-08-02 08:50:38 -0300

    02/08 - atualização de view

--- serial_data/views.py
+++ serial_data/views.py
@@ -3,20 +3,20 @@ from django.http import JsonResponse
 import serial
 
 def atualizar_variavel(request):
-    while True:
-        porta_serial = 'COM4' 
-        taxa_transmissao = 9600
+    
+    porta_serial = 'COM7' 
+    taxa_transmissao = 9600
 
-        arduino = serial.Serial(porta_serial, taxa_transmissao)
+    arduino = serial.Serial(porta_serial, taxa_transmissao)
 
-        try:
-            linha = arduino.readline().decode().strip()
-            valor_analogico = int(linha)
-            frase_view = f"Valor analógico lido: {valor_analogico}"
-        except serial.SerialException:
-            frase_view = "Aguardando leitura..."
+    try:
+        linha = arduino.readline().decode().strip()
+        valor_analogico = int(linha)
+        frase_view = f"Valor analógico lido: {valor_analogico}"
+    except serial.SerialException:
+        frase_view = "Aguardando leitura..."
             
-        return render(request, 'dashboard.html', {'frase_view': frase_view})
+    return render(request, 'dashboard.html', {'frase_view': frase_view})
 
 def dashboard(request):
     return render(request, 'dashboard.html')
