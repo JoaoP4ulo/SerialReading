d5e6ee678319da8a46a071647c7a32895d144259
JoaoPaulo <jppelioterio@gmail.com>
2023-08-02 11:33:57 -0300

    agr ta funfando a taxa de attt falta logica serial

--- serial_data/views.py
+++ serial_data/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-import serial
+import random
+import requests
+import json
 
 def atualizar_variavel(request):
-    
-    porta_serial = 'COM7' 
-    taxa_transmissao = 9600
-
-    arduino = serial.Serial(porta_serial, taxa_transmissao)
-
-    try:
-        linha = arduino.readline().decode().strip()
-        valor_analogico = int(linha)
-        frase_view = f"Valor analógico lido: {valor_analogico}"
-    except serial.SerialException:
-        frase_view = "Aguardando leitura..."
-            
+    frase_view = random.randint(1, 100)
+    print(frase_view)
     return render(request, 'dashboard.html', {'frase_view': frase_view})
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
