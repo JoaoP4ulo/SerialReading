493b7ebb9461fed280fa1ad9912638e32e876f5a
Joao Paulo <jppelioterio@gmail.com>
2023-08-03 13:04:35 -0300

    master rev2

--- serial_data/views.py
+++ serial_data/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 import random
 import requests
 import json
+import serial 
 
 def atualizar_variavel(request):
-    frase_view = random.randint(1, 100)
-    print(frase_view)
-    return render(request, 'dashboard.html', {'frase_view': frase_view})
+
+    porta_serial = 'COM4'
+    taxa_transmissao = 9600
+
+    arduino = serial.Serial(porta_serial, taxa_transmissao)
+
+    try:
+        
+            # Leia a linha recebida da porta serial do Arduino
+        linha = arduino.readline().decode().strip()
+            
+            # Converta o valor para inteiro (ou float, se necessário)
+        valor_analogico = int(linha)
+            
+            # Faça o que quiser com o valor lido (exemplo: imprimir na tela)
+
+    except serial.SerialException:
+        valor_analogico = f"Porta {porta_serial} não reconhecida"
+    
+
+    return render(request, 'dashboard.html', {'frase_view': valor_analogico})
 
